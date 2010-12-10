7ed7b6050b4d5623ad81abece45e9cffbd96cf9a
Garrett Grimm <grimmdude@Garrett-Grimms-iMac.local>
2010-12-10 14:08:08 -0800

    updates

--- raphael_guitar.js
+++ raphael_guitar.js
@@ -4,17 +4,19 @@ window.onload = function () {
 	// ID of containing Div
 	var div_id = 'guitar';
 	
+	var neck_length = 700;
+	var neck_height = 100;
+	
 	//------------------------------------------------------------
 
 	// Creates canvas 700 x 200 in above defined div
 	var paper = Raphael(div_id, 2000, 200);
-	
-	var neck_length = 700
-	var scale_length = neck_length/.95;
+
+	var scale_length = neck_length;
 	var multiplier = 17.817;
 	
-	// neck outline
-	var neck = paper.rect(0,0,neck_length,100);
+	// Neck
+	var neck = paper.rect(0,0,neck_length,neck_height);
 	
 	var last_fret = 0;
 	var frets = [];
@@ -23,12 +25,27 @@ window.onload = function () {
 		var scale_formula = ((scale_length-last_fret)/multiplier)+last_fret;
 		
 		// add the fret in the proper location
-		frets[i] = paper.rect(scale_formula,0,3,100);
+		frets[i] = paper.rect(scale_formula,0,3,neck_height);
+		
+		// get some position markers in there
+		if (i==3 || i ==5 || i ==7 || i==9) {
+			var fret_markers = paper.circle(scale_formula, neck_height/2, 8);
+			
+		}
 		
 		// define the last fret distance for the next go around
 		last_fret = ((scale_length-scale_formula)/multiplier)+scale_formula;
 	}
 	
+	//strings
+	for (i=1;i<=6;i++) {
+		// string paths here
+		//
+
+	}
+	
 	// highlight fret example
 	frets[1].attr("fill", "yellow");
+
+
 };
